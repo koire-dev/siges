88d36377085a98e80c7125f69439c611e8a9bb46
MylenaMeli <87312977+MylenaMeli@users.noreply.github.com>
2023-05-30 05:48:46 +0100

    Bulletin sequentiel

--- src/app/pages/principalVice/principalVice-studynote/studynotes02/studynotes02.component.ts
+++ src/app/pages/principalVice/principalVice-studynote/studynotes02/studynotes02.component.ts
@@ -23,4 +23,16 @@ export class Studynotes02Component {
       PDF.save('bulletin_de_notes.pdf');
     });
   }
+  public open(): void {
+    let DATA: any = document.getElementById('sequence');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('bulletin_sequentiel.pdf');
+    });
+  } 
 }
